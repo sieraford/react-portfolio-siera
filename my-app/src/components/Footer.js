2ab83f47412130a79240a67698b748f8956a72c2
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2023-03-16 21:53:53 -0700

    add footer icons and active tab styling

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub,faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub,faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function Footer() {
   return (
     <footer className="footer">
-      <a className="m-2" href="https://github.com/sieraford">
+      <a className="m-2" href="https://github.com/sieraford" target="_blank">
         <FontAwesomeIcon icon={faGithub} />
       </a>
-      <a className="m-2" href="https://www.linkedin.com/in/siera-ford">
+      <a className="m-2" href="https://www.linkedin.com/in/siera-ford" target="_blank">
         <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a className="m-2" href="https://twitter.com/siera_ford" target="_blank">
+        <FontAwesomeIcon icon={faTwitter} />
       </a>
     </footer>
   );
--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Navigation() {
+
   return (
     <Navbar bg="light" expand="lg">
     <Container>
@@ -11,10 +13,18 @@ function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/about-me">About Me</Nav.Link>
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link href="/resume">Resume</Nav.Link>
+          <LinkContainer to="/about-me">
+            <Nav.Link>About Me</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/portfolio">
+            <Nav.Link>Portfolio</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/contact">
+            <Nav.Link>Contact</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/resume">
+            <Nav.Link>Resume</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Container>
